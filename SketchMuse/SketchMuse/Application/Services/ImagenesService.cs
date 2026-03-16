ceef70b2422ac129e9f067e54310e993ebcaae5e
RaquelLastre <rlasarr2402@g.educaand.es>
2026-03-16 12:44:46 +0100

    Fallback a Unsplash

--- SketchMuse/SketchMuse/Application/Services/ImagenesService.cs
+++ SketchMuse/SketchMuse/Application/Services/ImagenesService.cs
@@ -6,13 +6,24 @@ namespace SketchMuse.Application.Interfaces
     public class ImagenesService: IImagenesService
     {
         private readonly BingImageService _bingImageService;
+        private readonly UnsplashService _unsplashservice;
         public ImagenesService(BingImageService bingService) {
             _bingImageService = bingService; 
         }
 
         public async Task<List<ImagenDTO>> PedirImagenes(string query, int count)
         {
-            var imagenes = await _bingImageService.LlamadaApiBing(query, count);
+            var imagenes = new List<ImagenDTO>();
+            await _unsplashservice.LlamadaApiBing(query, count);
+           /* try
+            {
+                await _bingImageService.LlamadaApiBing(query, count);
+            }
+            catch
+            {
+                await _unsplashservice.LlamadaApiBing(query, count);
+            }*/
+             
             return imagenes;
         }
     }
--- SketchMuse/SketchMuse/Infrastructure/ExternalApis/UnsplashService.cs
+++ SketchMuse/SketchMuse/Infrastructure/ExternalApis/UnsplashService.cs
@@ -2,5 +2,47 @@
 {
     public class UnsplashService
     {
+         private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+
+        public UnsplashService(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+        }
+
+        public async Task<List<ImagenDTO>> LlamadaApiUnsplash(string textoBusqueda, int numImagenes)
+        {
+            var apiKey = _config["UnsplashApi:ApiKey"];
+            string busquedaSinEspacios = Uri.EscapeDataString(textoBusqueda);
+            var url = $"https://api.unsplash.com/search/photos?query={busqueda}&per_page={numImagenes}&client_id={apiKey}";
+
+            var response = await _httpClient.GetAsync(url);
+            //comprueba que la respuesta sea 200-299 y si no lo es lanza una excepción
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var document = JsonDocument.Parse(json);
+
+            if (!document.RootElement.TryGetProperty("images_results", out JsonElement listaImagenes))
+            {
+                Console.WriteLine("No se encontraron resultados: " + json);
+                return new List<ImagenDTO>();
+            }
+
+            var imagenes = new List<ImagenDTO>();
+
+            foreach (var img in listaImagenes.EnumerateArray())
+            {
+                imagenes.Add(new ImagenDTO
+                {
+                    Url = img.GetProperty("urls").GetProperty("raw").GetString(),
+                    Titulo = img.TryGetProperty("alt_description", out JsonElement titleEl) ? titleEl.GetString(): ""
+                });
+            }
+
+            return imagenes;
+        }
     }
 }
